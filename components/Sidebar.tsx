f906cfb22cb0237515ad1bb77b32574da2569442
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-06-03 22:39:58 -0500

    FIxed section className for sidebar, completely deleted public folder

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,7 +9,7 @@ import { cn } from "@/lib/utils";
 const Sidebar = () => {
   const pathname = usePathname();
   return (
-    <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 x-white max-sm:hidden lg:w-[264px]">
+    <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
       <div className="flex flex-1 flex-full gap-6">{[]}</div>
       {sidebarLinks.map((link) => {
         const isActive =
