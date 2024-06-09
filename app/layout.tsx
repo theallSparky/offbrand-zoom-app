fcfd44a213f7e65d059c492bbd4211533d9892f9
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-06-09 16:54:52 -0500

    Added signup, changed appearance of clerk provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { useContext } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +17,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <ClerkProvider>
+      <ClerkProvider
+        appearance={{
+          layout: {
+            logoImageUrl: "/icons/yoom-logo.svg",
+            socialButtonsVariant: "iconButton",
+          },
+          variables: {
+            colorText: "#fff",
+            colorPrimary: "#0E7859",
+            colorBackground: "#1c1f2e",
+            colorInputBackground: "#252a41",
+            colorInputText: "#fff",
+          },
+        }}
+      >
         <body className={`${inter.className} bg-dark-2`}>{children}</body>
       </ClerkProvider>
     </html>
